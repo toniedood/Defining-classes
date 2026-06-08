fe2d7d143b0cb2813d77402d92eb6311004faec5
toniedood <toniedood@gmail.com>
2026-06-08 16:52:55 +0300

    task 4 progress

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from forum_post import ForumPost
 
 post1 = ForumPost( "Steven", "How to add text in CLasses", 4)
+post1.add_replies("Great work!")
+post1.add_replies("Love, it!")
+
 post2 = ForumPost( "Anderson", "Have you mannaged?", 3000)
+#post2.add_replies("it was verry helpfull.")
 
 print(post1.view()) 
 print(post2.view())
