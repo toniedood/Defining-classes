2a441a3403826ae543b859af5781c056184600ac
Toni Usev <toniedood@gmail.com>
2026-06-22 16:38:32 +0300

    Deposit and withdraw functions added.

--- personal_account.py
+++ personal_account.py
@@ -1,29 +1,10 @@
-class BankAccount: 
-    def __init__ (self, owner_name, account_number, balance):
-        self.owner_name = owner_name
-        self._account_number = account_number
-        self.__balance = 0
+from general_account import GeneralAccount
 
-    def deposit (self, amount):
-        if amount > 0:
-            self.__balance += amount
-            return f"Deposit successful. New balance: {self.__balance}"
-        else:
-            return "Deposit amount must be positive."
-    
-    def withdraw (self, amount):
-        if amount > self.__balance:
-            return "Insufficient funds."
-        elif amount <=0:
-            return "Withdrawal amount must be positive."
-        else:
-            self.__balance -= amount
-            return f"Withdrawal successful. New balance: {self.__balance}"
-        
-    def get_balance (self):
-        return f'Current balance is: {self.__balance}'
+class PersonalAccount(GeneralAccount):
+    def __init__ 
+        super().__init__(owneR_name, account_name, balance):
     
     def get_account_info (self):
-        return f"Account number: {slef._account_number}, Owner: {self.owone_name}"
-
-
+        return f'Persona account numer {slef.account_number}, Owner: {self.owner_name}. Current balance is: {self.__balance}'
+    
+    def 
